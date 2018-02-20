dfb95d1c1f2ac24cce5e22fdb03368ef24a8731f
Noctaum <Noctaum@users.noreply.github.com>
2018-02-20 09:10:43 +0300

    Task 2 - love-triangel answer

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+// your implementation
+  	let count = 0;
+  	for (let i = 0; i < preferences.length; i++){
+  		let loveFirst, loveSecond, loveThitd;
+  		loveFirst = preferences[i];
+  		if (checkExist(loveFirst)){
+  			loveSecond = preferences[loveFirst-1];
+  			if (checkExist(loveSecond)){
+  				loveThitd = preferences[loveSecond-1];
+  				if (loveThitd === i+1 && loveSecond != i+1) count++;
+  			}
+  		}
+	}
+	
+  	function checkExist(num){
+  		return (preferences.length+1) > num;
+  	}
+  	return count/3;
 };
